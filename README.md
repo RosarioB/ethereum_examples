18450837e59da991dfe643402cd17c1dc3815a21
Rosario Borgesi <borgesiros@gmail.com>
2023-09-09 23:02:38 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ethereum_examples
-Various examples of operations performed on the Ethereum blockachain
+# sending_ethers
+This is an example on how we can use the ethers library to send ethers from one Ethereum account to another.
+
+Before using the program in the file: `\ethereum_examples\node_modules\ethers\package.json` in the section export we must add:
+
+```
+"./lib.commonjs/utils": {
+        "import": "./lib.commonjs/utils/index.js",
+        "default": "./lib.commonjs/utils/index.js"
+},
+    "./lib.commonjs/providers": {
+        "import": "./lib.commonjs/providers/index.js",
+        "default": "./lib.commonjs/providers/index.js"
+}
+```
+
+Then we need to insert our information in the `.env` and in the `main.ts` file.
+
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import ethers, { Wallet } from "ethers";
+import { parseUnits } from "ethers/lib.commonjs/utils" ;  
+import { InfuraProvider } from "ethers/lib.commonjs/providers";
+import 'dotenv/config'
+
+const main = async () => {
+  
+  const network: string = process.env.ETHEREUM_NETWORK ?? "sepolia";
+  const provider: InfuraProvider = new InfuraProvider(
+    network,
+    process.env.INFURA_PROJECT_ID,
+    process.env.API_KEY_SECRET
+  );
+
+  const signer = new Wallet(process.env.SIGNER_PRIVATE_KEY ?? '', provider);
+
+  const limit: bigint = await provider.estimateGas({
+    from: signer.address,
+    to: "to_address",
+    value: parseUnits("0.0000001", "ether"),
+  });
+
+  const tx = await signer.sendTransaction({
+    from: "from_address",
+    to: "to_address",
+    value: parseUnits("0.0000001", "ether"),
+    gasLimit: limit,
+    nonce: await signer.getNonce(),
+    maxPriorityFeePerGas: parseUnits("1", "gwei"),
+    chainId: 11155111,
+  });
+  console.log("Mining transaction...");
+  console.log(`https://${network}.etherscan.io/tx/${tx.hash}`);
+  
+  const receipt: ethers.ethers.TransactionReceipt | null = await tx.wait();
+  
+  if(receipt != null) {
+    console.log(`Mined in block ${receipt.blockNumber}`);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+})
